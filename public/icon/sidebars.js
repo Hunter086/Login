708fe01cd9de9ec894b14f6975bddbb425302cfa
Hunter086 <85256492+Hunter086@users.noreply.github.com>
2021-09-17 14:57:19 -0300

    update 3.4

--- public/icon/sidebars.js
+++ public/icon/sidebars.js
@@ -1,13 +1,3 @@
-/* global bootstrap: false */
-(function () {
-  'use strict'
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-  tooltipTriggerList.forEach(function (tooltipTriggerEl) {
-    new bootstrap.Tooltip(tooltipTriggerEl)
-  })
-})()
-var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
-var collapseList = collapseElementList.map(function (collapseEl) {
-  return new bootstrap.Collapse(collapseEl)
+$('#dropdown').collapse({
+  toggle: false
 })
-
